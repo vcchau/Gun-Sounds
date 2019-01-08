088cd34c0772d16cc6ea93135a2356ff01d0f48e
Victor Chau <vcchau@gmail.com>
2019-01-07 21:33:52 -0600

    working changes

--- app/src/main/java/com/example/victor/gunsounds/MainActivity.java
+++ app/src/main/java/com/example/victor/gunsounds/MainActivity.java
@@ -6,19 +6,17 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Build;
-import android.provider.MediaStore;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,16 +26,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private SensorManager manager;
     private Sensor accelerometer;
-    private ImageButton button;
+    private ToggleButton toggleButton;
     private SoundPool soundPool;
-    private int sound1;
     private int numGuns = 1;
+    private int[] sounds = new int[numGuns];
     private TextView xView;
     private TextView yView;
     private TextView zView;
     private float previousY;
     private float previousX;
     private boolean loaded;
+    private boolean tiltFireOn;
 
 
     @Override
@@ -47,14 +46,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // Find manual fire button
-        button = findViewById(R.id.imageButton);
+        // Keep screen on while the app is open
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Identify toggle button
+        toggleButton = findViewById(R.id.toggleButton);
 
         // Initialize SensorManager and accelerometer
         manager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
         accelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
 
+        // Verify accelerator exists
+        if (accelerometer != null) {
+            manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+            tiltFireOn = true;
+        }
+        else {
+            tiltFireOn = false;
+            toggleButton.setEnabled(false);
+        }
+
+        // Set toggle button to match
+        toggleButton.setChecked(tiltFireOn);
 
         // Create soundpool based on Android version
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -70,11 +83,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         // Load sounds into sound pool
-        sound1 = soundPool.load(this, R.raw.gunshot_9_mm, 1);
+        sounds[0] = soundPool.load(this, R.raw.gunshot_9_mm, 1);
 
-        xView = findViewById(R.id.textView);
-        yView = findViewById(R.id.textView2);
-        zView = findViewById(R.id.textView3);
+//        xView = findViewById(R.id.textView);
+//        yView = findViewById(R.id.textView2);
+//        zView = findViewById(R.id.textView3);
 
         // Load the gun
         loaded = true;
@@ -82,61 +95,68 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        /* X and Z directional changes
-        *  -2 < x < 2
-        *  Phone placed horizontal with some room for error
-        *  ______________
-        *
-        *  -3 < z < 3
-        *  Phone placed vertical up/down with some room for error
-        *     |
-        *     |
-        *     |
-        *     |
-        *
-        *  Fire based on phone's orientation */
-        float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
+        if (tiltFireOn) {
+            /* X and Z directional changes
+             *  -2 < x < 2
+             *  Phone placed horizontal with some room for error
+             *  ______________
+             *
+             *  -3 < z < 3
+             *  Phone placed vertical up/down with some room for error
+             *     |
+             *     |
+             *     |
+             *     |
+             *
+             *  Fire based on phone's orientation */
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
 
-//        xView.setText("X: " + x);
-//        yView.setText("Y: " + y);
-//        zView.setText("Z: " + z);
+//            xView.setText("X: " + x);
+//            yView.setText("Y: " + y);
+//            zView.setText("Z: " + z);
 
-        // Horizontal firing
-        if (-2 < x && x < 1.5) {
-            // Check phone's previous orientation before firing
-            if ((y > previousY) && (y - previousY > 2)) {
-                fire();
+            // Horizontal firing
+            if (-2 < x && x < 1.5) {
+                // Check phone's previous orientation before firing
+                if ((y > 0) && (y - previousY > 2)) {
+                    fire();
+                }
             }
-        }
-        // Phone sideways firing
-        else if (-3 < z && z < 3) {
-            if (previousX > x && previousX - x > 2) {
-                fire();
+            // Phone sideways firing
+            else if (-3 < z && z < 3) {
+                if ((y - previousY > 2)) {
+                    fire();
+                }
             }
-        }
 
-        previousY = y;
-        previousX = x;
+            previousY = y;
+            previousX = x;
+        }
+    }
+
+    // Toggle tilt fire
+    public void toggleTilt(View view) {
+        tiltFireOn = !tiltFireOn;
     }
 
     // On click firing
     public void fire(View view) {
-        soundPool.play(sound1, 1, 1, 0, 0, 1);
+        soundPool.play(sounds[0], 1, 1, 0, 0, 1);
     }
 
     // On tilt firing
     public void fire() {
         if (loaded) {
-            soundPool.play(sound1, 1, 1, 0, 0, 1);
+            soundPool.play(sounds[0], 1, 1, 0, 0, 1);
             loaded = false;
             new Timer().schedule(new TimerTask() {
                 @Override
                 public void run() {
                     loaded = true;
                 }
-            }, 150);
+            }, 100);
             // Replace 150 with a variable denoting a certain weapon's firing rate
         }
     }
